b4732949c3472dbe5f0f20aabb3301a66eef8e3d
Marcel Beltz <marcel@beltz.info>
2015-09-15 15:04:44 +0200

    brew install angefangen

--- bin/install.sh
+++ bin/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
 
-dir=~/.dotfiles
-olddir=~/.dotfiles/bak
+dir=~/dotfiles
+olddir=~/dotfiles/bak
 files="vimrc vim gitconfig tmux.conf"
 
 echo "Creating $olddir for backup ..."
@@ -11,10 +11,16 @@ mkdir -p $olddir
 cd $dir
 
 for file in $files; do
-    mv ~/.$file $olddir
+    if [ -f  ~/.$file ]; then
+        mv ~/.$file $olddir/$file
+    fi
+
     echo "Creating symlink to $file in home directory"
     ln -s $dir/$file ~/.$file
 done
 
+brew_install () {
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+}
 
 
